78956f0704b47261215498d5b128f3afbfc56428
Haeun <84301295+haeun9988@users.noreply.github.com>
2025-03-06 21:31:19 +0900

    Delete backend/src/main/java/com/example/Flicktionary/domain/series/repository/SeriesRepository.java

--- backend/src/main/java/com/example/Flicktionary/domain/series/repository/SeriesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Flicktionary.domain.series.repository;
-
-import com.example.Flicktionary.domain.series.domain.Series;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeriesRepository extends JpaRepository<Series, Long> {
-}
