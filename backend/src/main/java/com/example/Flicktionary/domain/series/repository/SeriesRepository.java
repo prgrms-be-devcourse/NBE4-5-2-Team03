4ecf228dd00ce504350c0446d1511d36b9728947
Haeun <84301295+haeun9988@users.noreply.github.com>
2025-03-06 17:49:28 +0900

    Delete backend/src/main/java/com/example/Flicktionary/domain/series/repository/SeriesRepository.java

--- backend/src/main/java/com/example/Flicktionary/domain/series/repository/SeriesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Flicktionary.domain.series.repository;
-
-import com.example.Flicktionary.domain.series.entity.Series;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
