19b2f2b0ef55a09d556b644b19a70ad68ec85d96
Haeun <84301295+haeun9988@users.noreply.github.com>
2025-03-09 21:07:00 +0900

    fix: 수정

--- frontend/src/app/movies/[movieId]/reviews/page.tsx
+++ frontend/src/app/movies/[movieId]/reviews/page.tsx
@@ -1,15 +1,1 @@
-"use client";
 
-import { useEffect, useState } from "react";
-
-interface Review {
-    id: number;
-    nickname: string;
-    rating: number;
-    content: string;
-}
-
-export function MovieReviewsPage() {
-    const { movieId } = useParams();
-    
-}
